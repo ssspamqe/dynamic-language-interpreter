e2c47443ee489bc40d7b041622ad1230d3cfd776
Stepan Dementev <dementevssstepan@gmail.com>
2025-09-10 20:54:48 +0300

    feat: implement token factory

--- src/main/java/ru/innopolis/interpeter/lexer/tokens/TokenFactory.java
+++ src/main/java/ru/innopolis/interpeter/lexer/tokens/TokenFactory.java
@@ -7,10 +7,10 @@ public class TokenFactory {
     public Token getTokenByCode(Code code, String rawToken, Span span) {
         return switch (code) {
             case INT_LITERAL -> new IntegerToken(span, Long.parseLong(rawToken));
-            case REAL_LITERAL -> new RealToken(span, Double.parseDouble(rawToken)),
+            case REAL_LITERAL -> new RealToken(span, Double.parseDouble(rawToken));
             case STRING_LITERAL -> new StringToken(span, rawToken);
-        }
-
+            case IDENTIFIER -> new IdentifierToken(span, rawToken);
+            default -> new Token(span, code);
+        };
     }
-
 }
