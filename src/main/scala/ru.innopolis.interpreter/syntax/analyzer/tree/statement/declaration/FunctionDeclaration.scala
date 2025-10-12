3701dbbcb449c97476218ff2fc8303dd221a805b
Dementev Stepan <dementevssstepan@gmail.com>
2025-10-12 20:23:23 +0300

    feat: remove unused dtos

--- src/main/scala/ru.innopolis.interpreter/syntax/analyzer/tree/statement/declaration/FunctionDeclaration.scala
+++
@@ -1,5 +0,0 @@
-package ru.innopolis.interpreter.syntax.analyzer.tree.statement.declaration
-
-import ru.innopolis.interpreter.syntax.analyzer.tree.statement.CodeBlock
-
-case class FunctionDeclaration(name: String, body:CodeBlock) extends Declaration(name)
